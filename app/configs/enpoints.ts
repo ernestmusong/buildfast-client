3dae2cb0153f7884918c0e4b069bee8abea8cf58
Musong <ernestakeh@gmail.com>
2026-08-03 12:50:30 +0100

    Add config file

---
+++ app/configs/enpoints.ts
@@ -0,0 +1,8 @@
+const endpoints = {
+    siteTitle: 'BETXXX',
+    apiVersion: 'v1',
+    siteUrl: 'https://builfastcm.netlify.app/',
+    apiUrl: 'http://localhost:5000/',
+}
+
+export default endpoints
---
+++ app/configs/global.ts
@@ -0,0 +1,9 @@
+const endpoints = {
+    register: 'auth/register',
+    login: '/auth/login',
+    apiVersion: 'v1',
+    siteUrl: 'https://builfastcm.netlify.app/',
+    apiUrl: 'http://localhost:5000/',
+}
+
+export default endpoints
